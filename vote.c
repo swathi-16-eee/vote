a6e1af27f4fb526edc390de436c8f12e52ea48c6
swathi-16-eee <78012122+swathi-16-eee@users.noreply.github.com>
2021-02-25 20:34:42 +0530

    created a program vote.c
    This file prints whether the person is eligible for voting based on his/her age.

---
+++ vote.c
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+ int main()
+ {
+ 	int age;
+ 	cout<<"Enter age:";
+ 	cin>>age;
+ 	if(age>=18)
+ 	cout<<"\nYou are eligible for voting";
+ 	else
+ 	cout<<"\nYou are not eligible for voting";
+ 	return 0;
+ }
